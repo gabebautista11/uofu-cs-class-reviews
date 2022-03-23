a82e7a7ba83bc656cec714efbe0a8b5f7796041e
Gabe Bautista <gabe.bautista1115@gmail.com>
2022-03-22 22:01:43 -0600

    removed auth

--- src/FirebaseData/FirebaseConfig.js
+++ src/FirebaseData/FirebaseConfig.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { browserLocalPersistence, getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,6 +20,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-const auth = getAuth();
-auth.setPersistence(browserLocalPersistence);
-export { app, analytics, auth };
+export { app, analytics };
