2fc2809be7f62fcbf38124b3d7f904a922ff0b59
Gabe Bautista <gabe.bautista1115@gmail.com>
2022-03-28 01:23:38 -0600

    changed header login to fit on mobile

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,9 +5,6 @@ import { getAuth } from "firebase/auth";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { setUser } from "../../redux/auth";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useState } from "react";
 
 const Header = () => {
   const { user } = useSelector((state) => state.auth);
@@ -47,37 +44,33 @@ const Header = () => {
           )}
         </div>
       </div>
-      {PhoneHeader()}
-    </div>
-  );
-};
-
-const PhoneHeader = () => {
-  const [isPressed, setIsPressed] = useState(false);
-  const onButtonClick = () => {
-    setIsPressed(!isPressed);
-  };
-  return (
-    <div className="phone-header-menu">
-      <div className="phone-header-grid">
-        <div className="hamburger-button-div">
-          <FontAwesomeIcon
-            icon={faBars}
-            className="hamburger-button"
-            onClick={() => onButtonClick()}
-          />
+      <div className="mobile-header">
+        <div className="phone-header-grid-div">
+          <div className="logo-div">
+            <Link to={"/"} className="home-page-link">
+              <h1 className="logo">csReviews</h1>
+            </Link>
+          </div>
+          <div className="sign-in-button-div">
+            {user == null ? (
+              <button
+                className="sign-in-button"
+                onClick={() => signInClicked()}
+              >
+                Sign In
+              </button>
+            ) : (
+              <button
+                className="sign-in-button"
+                onClick={() => logoutClicked()}
+              >
+                Sign Out
+              </button>
+            )}
+          </div>
         </div>
-        <div className="logo-div">
-          <Link to={"/"} className="home-page-link">
-            <h1 className="logo">csReviews</h1>
-          </Link>
-        </div>
+        <SearchBar className="search-bar" />
       </div>
-      {isPressed && (
-        <div className="phone-menu">
-          <h1>MENU</h1>
-        </div>
-      )}
     </div>
   );
 };
